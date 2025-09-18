88ef2cac208d4d3ce38309b8bb109d516ae1b384
Joachim Riise <joachimriise@gmail.com>
2025-09-18 16:16:46 +0200

    Add ADS-B proxy to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
+      '/adsb-proxy': 'http://127.0.0.1:3001',
       '/adsb-proxy': {
         target: 'http://localhost:3001',
         changeOrigin: true,
